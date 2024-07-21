844674b4427ef5d2dbd15084f8857d1983245f4f
Vrana710 <73100366+Vrana710@users.noreply.github.com>
2024-07-21 12:38:40 +0200

    improve card item html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,32 +1,44 @@
 import json
 
 def load_data(file_path):
-    """Loads a JSON file"""
+    """ Loads a JSON file """
     with open(file_path, "r") as handle:
         return json.load(handle)
 
-def serialize_animal(animal):
-    output = '<li class="cards__item">\n'
-    output += f'<div class="card__title">{animal["name"]}</div>\n'
-    output += '<p class="card__text">\n'
-    output += f'<strong>Location:</strong> {animal["locations"][0]}<br/>\n'
-    if 'type' in animal:
-        output += f'<strong>Type:</strong> {animal["type"]}<br/>\n'
-    output += f'<strong>Diet:</strong> {animal["characteristics"]["diet"]}<br/>\n'
-    output += '</p>\n'
-    output += '</li>\n'
+def generate_animal_html(data):
+    """ Generate HTML string for animals with final design """
+    output = ''
+    for animal in data:
+        output += '<li class="cards__item">\n'
+        output += f'  <div class="card__title">{animal.get("name", "N/A")}</div>\n'
+        output += '  <p class="card__text">\n'
+        output += f'    <strong>Location:</strong> {animal.get("locations", [])[0] if animal.get("locations") else "N/A"}<br/>\n'
+        output += f'    <strong>Type:</strong> {animal.get("type", "N/A")}<br/>\n'
+        output += f'    <strong>Diet:</strong> {animal.get("characteristics", {}).get("diet", "N/A")}<br/>\n'
+        output += f'    <strong>Group:</strong> {animal.get("characteristics", {}).get("group","N/A")}<br/>\n'
+        output += f'    <strong>Lifespan:</strong> {animal.get("characteristics", {}).get("lifespan","N/A")}<br/>\n'
+        output += f'    <strong>Training:</strong> {animal.get("characteristics", {}).get("training","N/A")}<br/>\n'
+        output += f'    <strong>Temperament:</strong> {animal.get("characteristics", {}).get("temperament","N/A")}<br/>\n'
+        output += f'    <strong>Distinctive_feature:</strong> {animal.get("characteristics", {}).get("distinctive_feature","N/A")}<br/>\n'
+        output += f'    <strong>Average_litter_size:</strong> {animal.get("characteristics", {}).get("average_litter_size","N/A")}<br/>\n'
+        output += f'    <strong>Common_name:</strong> {animal.get("characteristics", {}).get("common_name","N/A")}<br/>\n'
+        output += f'    <strong>Slogan:</strong> {animal.get("characteristics", {}).get("slogan","N/A")}<br/>\n'
+        output += '  </p>\n'
+        output += '</li>\n'
     return output
 
-animals_data = load_data('animals_data.json')
 
-output = ""
-for animal in animals_data:
-    output += serialize_animal(animal)
+def update_html_template(template_path, output_path, animals_html):
+    """ Update HTML template with animal data """
+    with open(template_path, "r") as file:
+        html_content = file.read()
+    
+    html_content = html_content.replace('__REPLACE_ANIMALS_INFO__', animals_html)
+    
+    with open(output_path, "w") as file:
+        file.write(html_content)
 
-with open('animals_template.html', 'r') as file:
-    template = file.read()
-
-html_content = template.replace('__REPLACE_ANIMALS_INFO__', output)
-
-with open('animals.html', 'w') as file:
-    file.write(html_content)
+if __name__ == "__main__":
+    animals_data = load_data('animals_data.json')
+    animals_html = generate_animal_html(animals_data)
+    update_html_template('animals_template.html', 'animals.html', animals_html)
